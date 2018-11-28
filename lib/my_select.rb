ac51327f3382f3e1e58f6c7e43a565aaf9666410
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-28 02:51:36 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_col = []
+    while i < collection.length
+      new_num = yield collection[i]
+      new_col << new_num
+    end
+  new_col
+  
+
 end
